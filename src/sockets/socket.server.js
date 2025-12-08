613a03630a7ab9142cd2d3983c7f6ab1591a47aa
piyush ujgaokar <piyushujgaokar544@gmail.com>
2025-12-08 10:32:47 +0530

    Socket.io Connection Successfully

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,11 +1,47 @@
 const {Server}=require("socket.io")
+const cookie=require('cookie')
+const jwt=require('jsonwebtoken')
+const userModel = require("../models/user.model")
 
 function initSocketServer(httpServer){
 
     const io=new Server(httpServer,{})
 
-    io.on("connection",(socket)=>{
-        console.log("new socket Connection:",socket.id)
+    io.use(async (socket,next)=>{
+
+        const cookies=cookie.parse(socket.handshake.headers?.cookie || "");
+
+        if(!cookies){
+            next(new Error("Authentication Error:No token provided"))
+        }
+
+       try{
+
+         const decoded=jwt.verify(cookies.token,process.env.JWT_SECRET)
+
+         const user=await userModel.findById(decoded.id)
+
+         socket.user=user
+
+         next()
+
+
+
+       }catch(err){
+        next(new Error("Authentication Error: Invalid Token"))
+       }
+
+
+        
+
+    })
+
+    io.on("connection", (socket)=>{
+        socket.on("ai-message",async (messagePayLoad)=>{
+          
+           
+        })
+
     })
 }
 
